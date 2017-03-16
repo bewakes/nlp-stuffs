d274e45b420ff42d162794e1a7bfe6ed1dbd2d74
Bibekpandey <spirit_bibek@yahoo.com>
2017-03-16 22:57:07 +0000

    basic matrix creation and showing

--- NeuralNet/Matrix.hs
+++ NeuralNet/Matrix.hs
@@ -4,4 +4,25 @@ where
 import qualified Data.Vector as V
 import qualified Data.List as L
 
-data Matrix a = V.Vector (V.Vector a)
+data Matrix a = Matrix (V.Vector (V.Vector a))
+
+-- Size of a matrix
+size :: Matrix a -> (Int, Int)
+size (Matrix m) = (length m, length (V.head m))
+
+-- Create a matrix with given value and size
+createMatrix :: Int -> Int -> a -> Matrix a
+createMatrix r c v = Matrix $ V.fromList (
+        take r ( repeat (V.fromList ( take c ( repeat v)))))
+
+zeroMatrix :: Int -> Int -> Matrix Float
+zeroMatrix r c = createMatrix r c 0
+
+-- TODO: random element matrix
+
+instance (Show a) => Show (Matrix a) where
+    show (Matrix mat) = "[\n" ++ concatenated ++"]"
+
+        where concatenated = foldr (++) "" (map formatter (V.toList mat))
+              formatter = \x -> (" ["++ spaceSeparated x ++ "  ]\n")
+              spaceSeparated x = foldl (++) "" $ map (\y -> "  "++show y) (V.toList x)
