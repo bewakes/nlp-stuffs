1f39ecd475f305ce5e6f97b6e9113d4ab956c337
Bibekpandey <spirit_bibek@yahoo.com>
2017-03-21 07:19:25 +0000

    addition

--- NeuralNet/Matrix.hs
+++ NeuralNet/Matrix.hs
@@ -4,7 +4,7 @@ where
 import qualified Data.Vector as V
 import qualified Data.List as L
 
-data Matrix a = Matrix (V.Vector (V.Vector a))
+data Matrix a = Matrix (V.Vector (V.Vector a)) deriving (Eq)
 
 -- Size of a matrix
 size :: Matrix a -> (Int, Int)
@@ -26,3 +26,14 @@ instance (Show a) => Show (Matrix a) where
         where concatenated = foldr (++) "" (map formatter (V.toList mat))
               formatter = \x -> (" ["++ spaceSeparated x ++ "  ]\n")
               spaceSeparated x = foldl (++) "" $ map (\y -> "  "++show y) (V.toList x)
+
+-- addition of matrices
+instance (Num a) => Num (Matrix a) where
+(Matrix m1) + (Matrix m2) = Matrix $ V.map sumrows (V.zip m1 m2)
+    where sumrows (a, b) = V.map addZipped $ V.zip a b
+          addZipped (a, b) = a Prelude.+ b
+
+-- multiplication of matrices
+-- instance (Num a) => Num (Matrix a) where
+-- (Matrix m1) * (Matrix m2) = [ [mult x y | y <- cols m2] | x <- m1]
+--    where cols v = 
