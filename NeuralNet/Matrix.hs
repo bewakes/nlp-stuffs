4f9f81097ae10a100898f40056cef798b779b467
Bibek Pandey <bibek@janakitech.com>
2017-03-21 15:16:10 +0545

    transpose, identity done, buggy multiplication

--- NeuralNet/Matrix.hs
+++ NeuralNet/Matrix.hs
@@ -18,6 +18,16 @@ createMatrix r c v = Matrix $ V.fromList (
 zeroMatrix :: Int -> Int -> Matrix Float
 zeroMatrix r c = createMatrix r c 0
 
+identityMatrix :: Int -> Matrix Float
+identityMatrix 0 = Matrix (V.fromList [])
+identityMatrix n = Matrix (V.fromList $ getRows 0 n)
+    where getRows ind size
+            | ind >=size = []
+            | otherwise = V.fromList (oneAtPos ind size) : getRows (ind Prelude.+ 1) size
+          oneAtPos pos size = [booltoint (x == pos) | x <- [0..(size-1)]]
+          booltoint v | v==True = 1
+                      | otherwise = 0
+
 -- TODO: random element matrix
 
 instance (Show a) => Show (Matrix a) where
@@ -32,8 +42,18 @@ instance (Num a) => Num (Matrix a) where
 (Matrix m1) + (Matrix m2) = Matrix $ V.map sumrows (V.zip m1 m2)
     where sumrows (a, b) = V.map addZipped $ V.zip a b
           addZipped (a, b) = a Prelude.+ b
-
 -- multiplication of matrices
--- instance (Num a) => Num (Matrix a) where
--- (Matrix m1) * (Matrix m2) = [ [mult x y | y <- cols m2] | x <- m1]
---    where cols v = 
+mat1@(Matrix m1) * mat2@(Matrix m2) = [ [mult x y | y <- cols mat2] | x <- matToLst mat1]
+    where mult a b = sum $ V.map (\(x,y)->x Prelude.* y) $ V.zip a b
+          cols mat@(Matrix m) = matToLst $transpose mat
+          matToLst mat@(Matrix m) = V.toList m
+
+
+-- transpose of a matrix
+transpose :: Matrix a -> Matrix a
+transpose (Matrix m) = Matrix $ V.fromList $ getCols (V.toList m) 0
+    where getCols mat ind
+            | V.length m < ind = []
+            | otherwise = (V.fromList $ map (V.! ind) mat) : getCols mat (ind Prelude.+ 1)
+
+
