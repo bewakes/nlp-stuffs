e83e14068f481236718f1447dbe181ea61198cbf
Bibek Pandey <bibek@janakitech.com>
2017-03-24 18:34:35 +0545

    some functions

--- NeuralNet/Network.hs
+++ NeuralNet/Network.hs
@@ -1,4 +1,5 @@
 module Network (
+    Network(..)
 )
 where
 
@@ -20,15 +21,14 @@ data Network = Network {
 
 
 -- | feedforward function
--- |  - takes in Network and input
+-- |  - takes in Network and input(column vector)
 -- |  - returns output
-feedforward :: Network -> [Float] -> [Float]
-feedforward nw inp = feed inp (biases nw) (weights nw)
+feedforward :: Network -> Matrix Float -> Matrix Float
+feedforward nw (Matrix inp) = feed inp (biases nw) (weights nw)
     where feed ip [] _ = ip
           feed ip _ [] = ip
           feed ip (b:bs) (w:ws) = feed (sigmoidMat ((w Matrix.* ip) Matrix.+ b)) bs ws
 
---
 -- | stochastic Gradient Descent
 -- |  - takes in: 
 -- |        training_data: a tuple (x, y), x being input vector and y being desired output
@@ -36,9 +36,26 @@ feedforward nw inp = feed inp (biases nw) (weights nw)
 -- |        minibatch_size: size of each minibatch as training all at once is slow
 -- |        eta: learning rate
 -- |        test_data: this is optional
-stochasticGD :: (Matrix m, Matrix m) -> Int -> Int -> Float -> Maybe (Matrix m, Matrix m) -> Network
-stochasticGD tr_d ep mb_sz et tst_d = runepochs ep
+--stochasticGD :: (Matrix m, Matrix m) -> Int -> Int -> Float -> Maybe (Matrix m, Matrix m) -> Network
+--stochasticGD tr_d ep mb_sz et tst_d = runepochs ep
 
 
 -- TODO: first write non dirty functions(SGD is dirty as it uses random)
 --  evaluate, back_prop, cost_derivative, sigmoid'(derivative), update_mini_batch
+
+-- | costDerivative
+-- |  - returns the vector of partial derivatives for the output activations
+costDerivative :: Matrix Float -> Matrix Float -> Matrix Float
+costDerivative outputActivations y = outputActivations Matrix.- y
+
+-- | evaluate
+-- |  - returns the number of test inputs for which the output is correct
+evaluate :: (Matrix Float, Matrix Float) -> Network -> Int
+evaluate (inputs, outputs) nw = V.sum $ V.map (\(x,y)-> boolToInt (x==y)) result
+    where boolToInt | True = 1 :: Int
+                    | False = 0 :: Int
+          result = V.map desiredActualTuple $ V.zip inputs outputs
+          desiredActualTuple (ip, op) = (feedforward nw ip, op)
+
+-- | backpropagate
+-- |  - returns tuple grad_b, grad_w representing gradient of C wrt wts and b
